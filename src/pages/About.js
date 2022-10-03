e25d98a388c27adb6f9b16bd78d8d10fa702727d
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-10-03 14:17:45 +0100

    Created About us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem in
+        tenetur omnis nesciunt facilis ipsum assumenda ex quibusdam quos
+        praesentium eius quo, perspiciatis cupiditate quis nam voluptatibus
+        libero totam sint corporis quaerat dignissimos! Sint asperiores ipsa
+        aliquid veniam exercitationem enim.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
